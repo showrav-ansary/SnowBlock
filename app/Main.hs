4d86b1340950b6c3e4bd88a5f14a5045b3de9d2e
showrav-ansary <showrav.ansary.bd@gmail.com>
2022-01-03 15:26:37 +0600

    Initial infrastructure

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,96 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE Rank2Types        #-}
 module Main where
 
-import Lib
+import qualified Control.Monad.State as S
+import           Data.Aeson          hiding (json)
+import qualified Data.ByteString
+import           Data.IORef
+import           Data.Monoid         ((<>))
+import           Data.String         (fromString)
+import           Data.Text           (Text, pack)
+import           GHC.Generics
+import           Lib
+import           Web.Spock.Config
+import           Web.Spock.Core
+
+newtype RunStateT s m = RunStateT { runStateT :: forall a. S.StateT s m a -> m a }
+
+type Blockchain = [Block]
+
+
+-- Adds a transaction
+addTransaction :: (Monad m) => Transaction -> S.StateT Blockchain m Blockchain
+addTransaction tx = do
+    blockChain <- S.get
+    let blockHead = Prelude.head blockChain -- Block head
+    let blockTail = Prelude.tail blockChain -- Block tail
+    let blockHead' = addBlockTransaction blockHead tx -- Add tx to the head of block
+    let blockChain' = ([blockHead'] ++ blockTail) -- New blockchain
+    S.put blockChain'
+    return [blockHead']
+
+
+-- Mines the block head
+--
+mine :: (Monad m) => S.StateT Blockchain m Blockchain
+mine = do
+    blockChain <- S.get -- Blockchain
+    let blockHead = Prelude.head blockChain -- Block head
+    let blockTail = Prelude.tail blockChain -- Block tail
+    let minedBlock = mineBlock blockHead 0 -- Mined block
+    let blockHead' = Block (index minedBlock) [] "" (hash minedBlock) Nothing -- New block head
+    let blockChain' = ([blockHead', minedBlock] ++ blockTail)
+    S.put blockChain'
+    return [minedBlock]
+
+
+-- Returns current block head
+current :: (Monad m) => S.StateT Blockchain m Blockchain
+current = do
+    blockChain <- S.get
+    return $ [Prelude.head blockChain]
+
+
+-- Gets current blockhash
+getBlockhash :: (Monad m) => String -> S.StateT Blockchain m Blockchain
+getBlockhash h = do
+    blockChain <- S.get
+    return $ filter (\b -> (hash b) == h) blockChain
+
+
+-- Gets entire blockchasin
+getBlockchain :: (Monad m) => S.StateT Blockchain m Blockchain
+getBlockchain = S.get >>= return
+
+
+-- StateT Monad to wrap the blockchainaround
+restartableStateT :: s -> IO (RunStateT s IO)
+restartableStateT s0 = do
+    r <- newIORef s0
+    return $ RunStateT $ \act -> do
+        s <- readIORef r
+        (x, s') <- S.runStateT act s
+        atomicModifyIORef' r $ const (s', x)
+
 
 main :: IO ()
-main = someFunc
+main = do
+    runner <- restartableStateT [genesisBlock]
+    runSpock 8080 $ spockT (runStateT runner) $ do
+        get "/" $ (S.lift getBlockchain) >>= json
+        get "current" $ (S.lift current) >>= json        
+        get "block" $ do
+            blockID <- param "blockID"
+            case blockID of
+              (Just h) -> (S.lift $ getBlockhash h) >>= json
+              _        -> text "please supply blockID"
+        post "mine" $ (S.lift mine) >>= json
+        post "addTransaction" $ do
+            sender <- param "sender"
+            to <- param "receiver"
+            description <- param "description"
+            case (sender, to, description) of
+              (Just s, Just r, Just d) -> (S.lift $ addTransaction (Transaction s r d)) >>= json
+              _                        -> text "missing/incorrect parameters!"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,63 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import qualified Crypto.Hash.SHA256     as SHA256
+import           Data.Aeson             hiding (json)
+import           Data.ByteString        (ByteString, append, pack)
+import qualified Data.ByteString.Base16 as Base16
+import           Data.ByteString.UTF8   (fromString, toString)
+import qualified Data.Word
+import           GHC.Generics
+import           Unsafe.Coerce          (unsafeCoerce)
+
+data Transaction = Transaction { sender   :: String
+                               , receiver     :: String
+                               , description :: String
+                               } deriving (Generic, Show)
+
+data Block = Block { index    :: Int  -- Index of the block
+                   , trnx      :: [Transaction] -- List of transactions in the block
+                   , hash     :: String -- Hash of the block
+                   , previousHash :: String -- Prev Hash of the block
+                   , nonce    :: Maybe Int -- Nonce of the block (proof of work)
+                   } deriving (Generic, Show)
+
+instance ToJSON Transaction
+
+instance FromJSON Transaction
+
+instance ToJSON Block
+
+instance FromJSON Block
+
+-- Hash (SHA256) our Block
+hashBlock :: Block -> String
+hashBlock (Block blockIndex blockTransaction blockHash previousHash _) = toString $ Base16.encode digest
+    where blockTransaction = foldr ((++) . show) "" blockTransaction
+          context = SHA256.updates SHA256.init $ fmap fromString [blockTransaction, previousHash]
+          digest = SHA256.finalize context
+
+-- Adds a transaction to the head of the block
+addBlockTransaction :: Block -> Transaction -> Block
+addBlockTransaction (Block i ts h p n) t = Block i (ts ++ [t]) h p n
+
+-- Mine the block
+mineBlock :: Block -> Int -> Block
+mineBlock b@(Block i t _ p _) n = case head pow of
+                                    '0' -> Block i t blockHash p (Just n)
+                                    _   -> mineBlock b (n + 1)
+    where blockHash = hashBlock b
+          context = SHA256.updates SHA256.init (fmap fromString [blockHash, show n, p])
+          pow = toString . Base16.encode $ SHA256.finalize context -- proof of work
+
+
+-- Our genesis block
+genesisBlock :: Block
+genesisBlock = Block blockIndex blockTransaction blockHash previousHash Nothing
+    where blockIndex = 0
+          blockTransaction = [Transaction "Showrav" "Snow" "What a fluffy ball you are!"]
+          blockHash = ""
+          previousHash = "000000000000000000000000000000000"
